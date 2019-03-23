3b6388c92fcd2662c61315637b2fd6365b5f3360
hanxifeng <hanxf36@chinaunicom.cn>
2019-03-23 14:42:25 +0800

    IDEA 整合github测试

--- Gaoxi-Analysis/src/main/java/com/gaoxi/analysis/GaoxiAnalysisApplication.java
+++ Gaoxi-Analysis/src/main/java/com/gaoxi/analysis/GaoxiAnalysisApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GaoxiAnalysisApplication {
 
+	//1111111
 	public static void main(String[] args) {
 		SpringApplication.run(GaoxiAnalysisApplication.class, args);
 	}
